f140e90bc2336f64ffd4f27cbee62fef97017010
Ajay <ajay@pixeloco.com.au>
2018-05-21 14:32:50 +1000

    Initial React Context Api Example

--- src/App.js
+++ src/App.js
@@ -58,11 +58,9 @@ class App extends Component {
        <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React 16.3 Context API Example</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
         <Family name={this.props.name}/>
       </div>
       </MyProvider>
